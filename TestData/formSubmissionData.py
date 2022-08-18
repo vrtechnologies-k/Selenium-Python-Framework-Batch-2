0a46bfd62c97ed180883d5c8c774be0d4d54219d
venkatakandula@excellarate.com <vrtestingtechnologies1529@gmail.com>
2022-08-18 08:47:59 +0530

    Parametarized test data

---
+++ TestData/formSubmissionData.py
@@ -0,0 +1,4 @@
+
+class formSubmissionData:
+
+    test_formSubmissionData = [{"Name":"Venkat","Email":"Venkat@gmail.com","Password":"Venkat@123","Gender":"Male","DOB":"01-08-1988"},{"Name":"Latha","Email":"Latha@gmail.com","Password":"Latha@123","Gender":"Female","DOB":"01-01-1994"}]
--- tests/test_formSubmission.py
+++ tests/test_formSubmission.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
+from TestData.formSubmissionData import formSubmissionData
 from pageObjects.HomePage import HomePage
 from tests.conftest import driver
 from utilities.BaseClass import BaseClass
@@ -13,17 +14,17 @@ from utilities.BaseClass import BaseClass
 
 class TestTwo(BaseClass):
 
-    def test_submitForm(self):
+    def test_submitForm(self,getData):
         self.driver.get("https://rahulshettyacademy.com/angularpractice/")
 
         homePage = HomePage(self.driver)
-        homePage.getName().send_keys("venkat")
-        homePage.getEmail().send_keys("venkat@gmail.com")
-        homePage.getPassword().send_keys("venkat@123")
+        homePage.getName().send_keys(getData["Name"])
+        homePage.getEmail().send_keys(getData["Email"])
+        homePage.getPassword().send_keys(getData["Password"])
         homePage.getCheckbox().click()
-        self.selectOptionByText(homePage.getGender(),"Male")
+        self.selectOptionByText(homePage.getGender(),getData["Gender"])
         homePage.getRadio().click()
-        homePage.getBday().send_keys("01-08-1988")
+        homePage.getBday().send_keys(getData["DOB"])
         homePage.getSubmit().click()
         time.sleep(2)
         sucMessage = homePage.getAlert().text
@@ -33,6 +34,10 @@ class TestTwo(BaseClass):
     def test_sample(self):
         print("sample test")
 
-    @pytest.fixture(params=[("venkat","venkat@gmail.com","venkat@123","Male","01-08-1988")])
+    #@pytest.fixture(params=[("venkat","venkat@gmail.com","venkat@123","Male","01-08-1988"),("Sreelatha","Sreelatha@gmail.com","Sreelatha@123","Female","01-01-1994")])
+    #def getData(self,request):
+        #return request.param
+
+    @pytest.fixture(params=formSubmissionData.test_formSubmissionData)
     def getData(self,request):
         return request.param
